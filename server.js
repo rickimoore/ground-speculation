f154fb9a1364826d993709679682485c09ebbce3
Ricki Moore <mavrik.development@gmail.com>
2019-05-28 15:16:51 +0200

    removed app.get from server test

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const serveStatic = require("serve-static")
 const path = require('path');
 const app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
-app.get('/.*/', function (req, res) {
-  res.sendFile(__dirname + '/dist/index.html')
-});
+// app.get('/.*/', function (req, res) {
+//   res.sendFile(__dirname + '/dist/index.html')
+// });
 const port = process.env.PORT || 8080;
 app.listen(port);
